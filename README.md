9b37f1518a14c677ffb6d7b714e4ba0f50e5aee6
Lucas Moreira <eskerlucas1@gmail.com>
2017-05-14 12:02:47 -0300

    :100: first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ngX Webpack Boilerplate
+
+It's very easy, just follow the steps below:
+
+`instal node v7`
+`clone this project`
+`npm i -g yarn`
+`yarn install`
+`npm start`
+`localhost:9000` 
+
+:boom:
---
+++ client/app.component.js
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+// import * as reset from 'eric-meyer-reset.scss/_reset.scss'
+// import * as styles from './styles/app.scss'
+
+@Component({
+  selector: 'app',
+  // styles: [`
+    // ${reset}
+    // ${styles}
+  // `],
+  template: `
+    <h1>Working 🤙</h1>
+  `
+})
+export class AppComponent {}
